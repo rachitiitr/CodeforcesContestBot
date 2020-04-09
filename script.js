3c9713ea75eded0d0d9eb858f13aa9835c17d4fb
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2020-04-09 21:11:33 +0530

    using: folder structure ./parsed/$contest_number/{A..E}
    Now all the files will be routed to seperate contest folders

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 var fs = require('fs');
 
+const CF_CODE = (process.env.CF_CONTEST).match(/(\d+)/)[0];
+const TARGET_DIR = __dirname + `/Parsed/` + `${CF_CODE}`;
+
+
 url = 'http://codeforces.com/contest/1256/problem/C'
 
 let getTestCaseFromProblemHtml = (dir, html) => {
 
-  fs.copyFileSync(`${dir}/../template.cpp`, `${dir}/sol.cpp`);
+  fs.copyFileSync(`${dir}/../template.cpp`, `${TARGET_DIR}/${dir}/sol.cpp`);
   data = [];
-  const $ = cheerio.load(html);
+  const $ = cheerio.load(html); 
   $('div.input').each((i, elem) => {
     data[i] = {
       ...data[i],
@@ -23,17 +27,17 @@ let getTestCaseFromProblemHtml = (dir, html) => {
   });
   console.log(data);
   data.forEach((test, i) => {
-    fs.writeFile(`${dir}/in${i}.txt`, test.input, function(err) {
+    fs.writeFile(`${TARGET_DIR}/${dir}/in${i}.txt`, test.input, function(err) {
       if(err) {
           console.log(err);
       }
-      console.log(`The file ${dir}/in${i}.txt was saved!`);
+      console.log(`The file ${TARGET_DIR}/${dir}/in${i}.txt was saved!`);
     }); 
-    fs.writeFile(`${dir}/out${i}.txt`, test.output, function(err) {
+    fs.writeFile(`${TARGET_DIR}/${dir}/out${i}.txt`, test.output, function(err) {
       if(err) {
           console.log(err);
       }
-      console.log(`The file ${dir}/out${i}.txt was saved!`);
+      console.log(`The file ${TARGET_DIR}/${dir}/out${i}.txt was saved!`);
     }); 
   })
   console.log(data);
@@ -41,9 +45,9 @@ let getTestCaseFromProblemHtml = (dir, html) => {
 
 function getTestCaseFromProblemUrl(url) {
   var dir = `./${url.substring(url.lastIndexOf('/')+1)}`;
-
-  if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+  var xdir = `${TARGET_DIR}` + `/` + `${dir}`;
+  if (!fs.existsSync(xdir)){
+      fs.mkdirSync(xdir, {recursive: true});
   }
 
   axios.get(url)
